1c61379c4032a9c9292074201f7e15baf86d1270
Valentin Roussel <valentin.rssl@gmail.com>
2024-12-17 09:36:48 +0100

    day 5: refactor

--- 05/check_updates.lua
+++ 05/check_updates.lua
@@ -1,54 +1,62 @@
 #!/usr/bin/env luajit
 
+local function check_pages_order(page1, page2, rules)
+	local rules1 = rules[page1] or {}
+	local rules2 = rules[page2] or {}
+
+	-- rules says page1 comes before page2
+	if rules1[page2] then
+		return true
+	-- rules says page2 comes before page1
+	elseif rules2[page1] then
+		return false
+	-- rules say nothing, so current order is fine
+	else
+		return false
+	end
+end
+
+local function is_update_valid(update, rules)
+	for i = 1, (#update - 1) do
+		for j = i + 1, #update do
+			local page_left = update[i]
+			local page_right = update[j]
+			-- if a rule says that page_right must be before page_left
+			if not check_pages_order(page_left, page_right, rules) then
+				return false
+			end
+		end
+	end
+	return true
+end
+
 -- parse rules
-local rules_by_page = {}
+local rules = {}
 while true do
 	local line = io.read("*l")
 	local a, b = line:match("(%d+)|(%d+)")
 	if not a or not b then
 		break
 	end
-    local rule = {a,b}
-    rules_by_page[a] = rules_by_page[a] or {}
-    rules_by_page[b] = rules_by_page[b] or {}
-    table.insert(rules_by_page[b], rule)
-    table.insert(rules_by_page[a], rule)
+	rules[a] = rules[a] or {}
+	rules[a][b] = true
 end
 
-local function check_rule(rule, idx_by_page)
-    local a_idx = idx_by_page[rule[1]]
-    local b_idx = idx_by_page[rule[2]]
-    if a_idx == nil or b_idx == nil then
-        return true
-    end
-    return a_idx < b_idx
-end
-
-local magic_count = 0
+local magic_number1 = 0
 while true do
 	local line = io.read("*l")
 	if line == nil then
 		break
 	end
-    local update = {}
-    local idx_by_page = {}
+	local update = {}
 	for page in line:gmatch("[^,]+") do
-        table.insert(update, page)
-        idx_by_page[page] = #update
-    end
-    for _, page in pairs(update) do
-        for _,rule in pairs(rules_by_page[page]) do
-            if not check_rule(rule, idx_by_page) then
-                goto next_update
-            end
-        end
-    end
+		table.insert(update, page)
+	end
 
-    -- all rules are respected
-    local middle_index = (#update + 1) / 2
-    magic_count = magic_count + update[middle_index]
-
-    ::next_update::
+	local middle_index = (#update + 1) / 2
+	if is_update_valid(update, rules) then
+		magic_number1 = magic_number1 + update[middle_index]
+	end
 end
 
-print(string.format("magic_count1: %d", magic_count))
+print(string.format("magic_number1: %d", magic_number1))
